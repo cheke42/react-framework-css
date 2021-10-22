946ad0687d1e7f063e3c6eec42e5f0e271a04997
Cheke <cheke@example.com>
2021-10-21 22:39:08 -0300

    Integrando Fws CSS

---
+++ src/components/MaterialUI.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MediaCard from './MaterialCard'
+import MaterialDrawer from './MaterialDrawer'
+
+export default function MaterialUI(){
+    return(
+        <>
+        <h2>Materia UI</h2> 
+        <MediaCard />
+        <MaterialDrawer />
+        </>
+    )
+}
